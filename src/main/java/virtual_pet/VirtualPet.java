b308703076bb8f41f1297d925802b5494c6e0e3c
Ben Williams <DeanPBR@gmail.com>
2020-05-18 22:23:54 -0400

    Adding grading feedback.

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,5 +1,14 @@
 package virtual_pet;
-
+/*
+ * I would like to see a few changes made for clean code's sake:
+ * 1 - I like hunger as a field name as it's a noun, the other names that are verbs (drink, play, and rest) I'm not a
+ *     fan of.  Use verbs for method names, as they describe behaviors.  Field names are better as nouns, as they are
+ *     things the class knows about. (Think 'thirst' instead of 'drink', 'tiredness' instead of 'rest')
+ * 2 - Separate the status display from the tick() into it's own method.  This way it can be called independently of the
+ *     tick.  It also keeps the tick with a single responsibility, advancing the game state.
+ * 3 - Make the 'getter' methods simply return the fields they relate to.
+ * 4 - Create methods that change the instance variables, like 'feed()', 'drink()', 'takeANap()', and 'play()'
+ */
 public class VirtualPet {
     private int hunger = 5;
     private int drink = 5;
